177461a796850edbecc2f3144b465e080567b58f
test <614515771@qq.com>
2022-05-03 17:55:27 +0800

    main

---
+++ static/js/login.js
@@ -0,0 +1,187 @@
+﻿//var url = "http://127.0.0.1"
+var url = "http://www.xigua77.xyz"
+
+
+$(function () {
+
+	$('#switch_qlogin').click(function () {
+		$('#switch_login').removeClass("switch_btn_focus").addClass('switch_btn');
+		$('#switch_qlogin').removeClass("switch_btn").addClass('switch_btn_focus');
+		$('#switch_bottom').animate({ left: '0px', width: '70px' });
+		$('#qlogin').css('display', 'none');
+		$('#web_qr_login').css('display', 'block');
+
+	});
+	$('#switch_login').click(function () {
+
+		$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
+		$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
+		$('#switch_bottom').animate({ left: '154px', width: '70px' });
+
+		$('#qlogin').css('display', 'block');
+		$('#web_qr_login').css('display', 'none');
+	});
+	if (getParam("a") == '0') {
+		$('#switch_login').trigger('click');
+	}
+
+});
+
+function logintab() {
+	scrollTo(0);
+	$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
+	$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
+	$('#switch_bottom').animate({ left: '154px', width: '96px' });
+	$('#qlogin').css('display', 'none');
+	$('#web_qr_login').css('display', 'block');
+
+}
+
+
+//根据参数名获得该参数 pname等于想要的参数名 
+function getParam(pname) {
+	var params = location.search.substr(1); // 获取参数 平且去掉？ 
+	var ArrParam = params.split('&');
+	if (ArrParam.length == 1) {
+		//只有一个参数的情况 
+		return params.split('=')[1];
+	}
+	else {
+		//多个参数参数的情况 
+		for (var i = 0; i < ArrParam.length; i++) {
+			if (ArrParam[i].split('=')[0] == pname) {
+				return ArrParam[i].split('=')[1];
+			}
+		}
+	}
+}
+
+
+var reMethod = "post",
+	pwdmin = 6;
+
+$(document).ready(function () {
+	$('#login').click(function () {
+
+		if ($('#u').val() == "") {
+			$('#u').focus().css({
+				border: "1px solid red",
+				boxShadow: "0 0 2px red"
+			});
+			$('#logintip').html("<font color='red'><b>×请输入密码用户名</b></font>");
+			return false;
+
+
+		} else {
+			$('#logintip').html("<font color='red'><b></b></font>");
+		}
+
+
+		if ($('#p').val() == "") {
+			$('#p').focus().css({
+				border: "1px solid red",
+				boxShadow: "0 0 2px red"
+			});
+			$('#logintip').html("<font color='red'><b>×请输入密码</b></font>");
+			return false;
+
+
+		} else {
+			$('#logintip').html("<font color='red'><b></b></font>");
+		}
+
+		console.log("login开始post")
+
+		$.ajax({
+			type: 'POST',
+			url: url+"/login",
+			async: false,
+			//data: "name=" + $("#u").val() + "pwd=" + $("#p").val(),
+			data: { name: $("#u").val(), pwd: $("#p").val() },
+			dataType: 'text',
+
+			success: function (result) {
+				console.log(result);
+				console.log("lonin回复=" + result);
+				if (result == "登陆成功") {
+					window.location.href = "../table2.html";
+					console.log("登陆成功");
+				} else {
+					window.alert(result);
+					console.log("登陆失败");
+				}
+			},
+		});
+
+
+
+
+
+	});
+
+	$('#reg').click(function () {
+
+		if ($('#user').val() == "") {
+			$('#user').focus().css({
+				border: "1px solid red",
+				boxShadow: "0 0 2px red"
+			});
+			$('#userCue').html("<font color='red'><b>×用户名不能为空</b></font>");
+			return false;
+		}
+
+
+
+		if ($('#user').val().length < 4 || $('#user').val().length > 16) {
+
+			$('#user').focus().css({
+				border: "1px solid red",
+				boxShadow: "0 0 2px red"
+			});
+			$('#userCue').html("<font color='red'><b>×用户名位4-16字符</b></font>");
+			return false;
+
+		}
+
+
+
+		if ($('#passwd').val().length < pwdmin) {
+			$('#passwd').focus();
+			$('#userCue').html("<font color='red'><b>×密码不能小于" + pwdmin + "位</b></font>");
+			return false;
+		}
+		if ($('#passwd2').val() != $('#passwd').val()) {
+			$('#passwd2').focus();
+			$('#userCue').html("<font color='red'><b>×两次密码不一致！</b></font>");
+			return false;
+		}
+
+
+
+		console.log("reg开始post")
+
+		$.ajax({
+			type: 'POST',
+			url: url+"/reg",
+			//data: "name=" + $("#u").val() + "pwd=" + $("#p").val(),
+			data: { name: $("#user").val(), pwd: $("#passwd").val() },
+			dataType: 'text',
+
+			success: function (result) {
+				console.log(result);
+				console.log("lonin回复=" + result);
+				if (result == "注册成功") {
+					window.alert(result);
+					window.location.href = "../index.html";
+					console.log("注册成功");
+				} else if(result == "用户名已经被注册！") {
+					$('#userCue').html("<font color='red'><b>用户名已经被注册！</b></font>");
+					console.log("用户名已经被注册！");
+				}
+			},
+		});
+
+	});
+
+
+});
---
+++ static/js/myws.js
@@ -0,0 +1,117 @@
+﻿// -----------------ws api -----------------------------
+// 1、提供接口myws_start(url)，简单启动ws。
+// 2、提供接口myws_send(data), 发送数据。
+// 3、支持自动重连,重连间隔5000ms。
+ 
+//可以在外部实现这几个方法，以便监听ws事件
+//function myws_onopen() {};
+//function myws_onmessage() {};
+//function myws_onerror() {};
+//function myws_onclose() {};
+ 
+// =================以下提供对外使用接口==================
+ 
+// 供外部启动ws，只需调用一次
+function myws_start(url) {
+    myws_start_init(url);
+}
+ 
+// 供外部发送数据。
+function myws_send(data) {
+    myws_send_data(data);
+}
+ 
+ 
+// =================以下代码，外部不需要关注==================
+// =================以下代码，外部不需要关注==================
+var url = "http://www.xigua77.xyz"
+var ws_url = "ws://" + "www.xigua77.xyz:9002";
+
+//var url = "http://127.0.0.1"
+//var ws_url = "ws://" + "127.0.0.1:9002";
+
+var myws = null;
+var myws_url_0 = "";
+var myws_init = false;
+var myws_isok = false;
+var myws_reconnect_interval_ms = 60*1000;
+var myws_isneedreconnect = false;
+ 
+var myws_onopen = null;
+var myws_onmessage = null;
+var myws_onerror = null;
+var myws_onclose = null;
+ 
+
+
+
+// 启动ws，只需调用一次
+function myws_start_init(url) {
+    if (true == myws_init) {
+        console.log("no need to repeat start..");
+        return;
+    }
+    myws_init = true;
+    myws_url_0 = url;
+    myws_connect();
+}
+ 
+// 供外部发送数据。
+function myws_send_data(data) {
+    if( true == myws_isok ) {
+        myws.send(data);
+    } else {
+        console.log("websocket is not ok..");
+    }
+}
+ 
+// 连接
+function myws_connect() {
+    if ("WebSocket" in window) {
+       console.log("您的浏览器支持 WebSocket!"); 
+       // 打开一个 web socket
+       myws = new WebSocket(myws_url_0);
+       myws.onopen = function() {
+            console.log(myws_url_0+"连接成功..."); 
+            if (null != myws_onopen) myws_onopen();
+       };
+       myws.onmessage  = function(evt) {
+            if (null != myws_onmessage) myws_onmessage(evt);
+       };
+       myws.onerror = function() {
+            console.log("连接错误..."); 
+            if (null != myws_onerror) myws_onerror();
+            myws_isok = false;
+            myws_isneedreconnect = true;
+       };
+       myws.onclose = function() {
+            console.log("连接已关闭..."); 
+            if (null != myws_onclose) myws_onclose();
+            myws_isok = false;
+            myws_isneedreconnect = true;
+       };
+       myws_isok = true;
+    } else {
+       // 浏览器不支持 WebSocket
+       console.log("您的浏览器不支持 WebSocket!");
+    }
+}
+ 
+function myws_reconnect() {
+    myws_connect();
+}
+ 
+function myws_checkconnect() {
+    if (false == myws_isok && true == myws_isneedreconnect) {
+        myws_isneedreconnect = false;
+        console.log("准备自动重连..."); 
+        myws_reconnect();
+    }
+}
+ 
+setInterval(function(){
+console.log("发送心跳..."); 
+    myws.send("ping");    
+myws_checkconnect();
+}, myws_reconnect_interval_ms);
+ 
